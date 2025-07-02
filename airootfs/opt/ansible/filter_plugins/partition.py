af9851d99a27fa6a42eafcc92ad79f0d788f34da
kasefuchs <kasefuchs@protonmail.com>
2025-07-02 17:16:13 +0300

    :construction: Add lvm to partition role

--- airootfs/opt/ansible/filter_plugins/partition.py
+++ airootfs/opt/ansible/filter_plugins/partition.py
@@ -8,7 +8,23 @@ def join_device_partition(device_tuple: Tuple[str, int]) -> str:
     return f"{device}p{part}" if re.search(r"\d$", device) else f"{device}{part}"
 
 
+def escape_lvm_name(name: str) -> str:
+    return name.replace("-", "--")
+
+
+def lvm_mapper_path(vg_lv: Tuple[str, str]) -> str:
+    vg, lv = vg_lv
+
+    escaped_vg = escape_lvm_name(vg)
+    escaped_lv = escape_lvm_name(lv)
+
+    return f"/dev/mapper/{escaped_vg}-{escaped_lv}"
+
+
 # noinspection PyMethodMayBeStatic
 class FilterModule:
     def filters(self) -> Dict[str, Any]:
-        return {"join_device_partition": join_device_partition}
+        return {
+            "join_device_partition": join_device_partition,
+            "lvm_mapper_path": lvm_mapper_path,
+        }
